38c79cdaa4ef7818135a4a76c262384a619420c3
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-11-25 23:11:48 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  get 'prototypes/index'
+  root to: "prototypes#index"
+  resources :prototypes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :comments, only: :create
+  end
+  resources :users, only: [:show]
 end
